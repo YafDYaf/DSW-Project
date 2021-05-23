a21f0c4a9fc120263f8b9de88fab01c837184b27
YafDYaf <82478048+YafDYaf@users.noreply.github.com>
2021-05-23 15:03:43 +0200

    log

--- app.js
+++ app.js
@@ -5,3 +5,15 @@ tl.to(".slider",{y: "-100%", duration:1.5, delay:0.5});
 tl.to('.intro',{y:"-100%",duration:1},"-=2");
 tl.fromTo('nav',{ opacity: 0},{ opacity: 1, duration:1},);
  
+
+
+document.getElementById("open_popup_btn").addEventListener("click", function(){
+    document.getElementsByClassName("popup_center")
+    [0].classList.add("active");
+})
+
+
+document.getElementById("dismiss_popup_btn").addEventListener("click", function(){
+    document.getElementsByClassName("popup_center")
+    [0].classList.remove("active");
+})
